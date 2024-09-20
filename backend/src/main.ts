ff707a05c22914cb74e2062b64b3bd86da7e18f7
Alexandre Marques Bulcao <ambulcao@gmail.com>
2024-09-20 20:38:53 +0100

    Enabling cors and releasing headers

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+
+    allowedHeaders: [
+      'Accept',
+      'Authorization',
+      'Content-Type',
+      'X-Requested-With',
+      'apollo-require-preflight',
+    ],
+    methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
+  })
+
+  app.use(cookieParser());
   await app.listen(3000);
 }
 bootstrap();
